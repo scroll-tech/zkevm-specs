dca350dd96a3a5c481d68832ad6404411326d7bc
SwordArt0820 <sendthefuture@outlook.com>
2022-01-16 15:44:10 +0800

    div spec finished.(md file not finished version)

---
+++ src/zkevm_specs/opcode/div.py
@@ -0,0 +1,35 @@
+from typing import Sequence
+
+from ..encoding import U8, is_circuit_code, U256, u8s_to_u64s
+
+
+def div_common(
+    dividend8s: Sequence[U8],
+    divisor8s: Sequence[U8],
+    quotient8s: Sequence[U8],
+    remainder8s: Sequence[U8],
+    v0: Sequence[U8],
+):
+    assert len(dividend8s) == len(divisor8s) == len(quotient8s) == len(remainder8s) == 32
+    assert len(v0) == 9
+    dividend64s = u8s_to_u64s(dividend8s)
+    divisor64s = u8s_to_u64s(divisor8s)
+    quotient64s = u8s_to_u64s(quotient8s)
+    remainder64s = u8s_to_u64s(remainder8s)
+
+    v0m = U256(0)
+    for i, u8 in enumerate(v0):
+        assert 0 <= u8 <= 255
+        v0m += u8 * (2 ** (8 * i))
+
+    t0 = divisor64s[0] * quotient64s[0]
+    t1 = divisor64s[0] * quotient64s[1] + divisor64s[1] * quotient64s[0]
+    t2 = divisor64s[0] * quotient64s[2] + divisor64s[1] * quotient64s[1] + divisor64s[2] * quotient64s[0]
+    t3 = divisor64s[0] * quotient64s[3] + divisor64s[1] * quotient64s[2] + divisor64s[2] * quotient64s[1] + divisor64s[3] * quotient64s[0]
+    assert v0m * (2 ** 128) == t0 + t1 * (2 ** 64) + remainder64s[0] + remainder64s[1] * (2 ** 64) - dividend64s[0] - dividend64s[1] * (2 ** 64)
+    assert 0 == v0m + t2 + t3 * (2 ** 64) +remainder64s[2] + remainder64s[3] * (2 ** 64) - dividend64s[2] - dividend64s[3] * (2 ** 64)
+
+
+@is_circuit_code
+def check_div(dividend8s, divisor8s, quotient8s, remainder8s, v0):
+    div_common(dividend8s, divisor8s, quotient8s, remainder8s, v0)
---
+++ tests/test_div.py
@@ -0,0 +1,40 @@
+from typing import Sequence, List
+
+import random
+
+from zkevm_specs.encoding import u256_to_u8s, U256, u8s_to_u64s
+from zkevm_specs.opcode.div import check_div
+
+def test_div():
+    dividend = random.randint(0, 2 ** 256)
+    divisor = random.randint(1, dividend)
+    quotient = dividend // divisor
+    remainder = dividend - divisor * quotient
+    
+    # a->divisor b->quotient c->dividend d->remainder
+    a8s = u256_to_u8s(U256(divisor))
+    b8s = u256_to_u8s(U256(quotient))
+    c8s = u256_to_u8s(U256(dividend))
+    d8s = u256_to_u8s(U256(remainder))
+    a64s = u8s_to_u64s(a8s)
+    b64s = u8s_to_u64s(b8s)
+    c64s = u8s_to_u64s(c8s)
+    d64s = u8s_to_u64s(d8s)
+    # t0 t1 t2 t3
+    t = [U256(0)] * 4
+    for total_idx in range(2):
+        rhs_sum = U256(0)
+        for a_id in range(0, total_idx + 1):
+            a_idx, b_idx = a_id, total_idx - a_id
+            tmp_a = a64s[a_idx] if len(a64s) >= a_idx + 1 else 0
+            tmp_b = b64s[b_idx] if len(b64s) >= b_idx + 1 else 0
+            t[total_idx] += tmp_a * tmp_b
+
+    # v0, v1
+    v = [U256(0)] * 2
+    v[0] = U256((t[0] + t[1] * (2 ** 64) + d64s[0] + d64s[1] * (2 ** 64) - c64s[0] - c64s[1] * (2 ** 64)) // (2 ** 128))
+    assert 0 <= v[0] <= (2 ** 66)
+
+    v0 = u256_to_u8s(v[0])[:9]
+
+    check_div(c8s, a8s, b8s, d8s, v0)
